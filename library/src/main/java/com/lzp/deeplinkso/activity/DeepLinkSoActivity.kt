b21bc2ed4b35ad099cd1e8ff2c7d426b893c0583
lizhipeng <lizhipeng@ugirls.com>
2018-12-10 11:57:23 +0800

    封装处理深度链接中转页面的逻辑

--- library/src/main/java/com/lzp/deeplinkso/activity/DeepLinkSoActivity.kt
+++ library/src/main/java/com/lzp/deeplinkso/activity/DeepLinkSoActivity.kt
@@ -1,15 +1,9 @@
 package com.lzp.deeplinkso.activity
 
-import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.lzp.deeplinkso.DeepLinkSoClient
 import com.lzp.deeplinkso.R
-import com.lzp.deeplinkso.bean.DeepLinkSoOption
-import com.lzp.deeplinkso.constants.DeepLinkSoConstant
-import com.lzp.deeplinkso.exception.DeepLinkSoFailedException
 import com.lzp.deeplinkso.utils.DeepLinkSoNavigator
-import com.lzp.deeplinkso.utils.Utils
 
 /**
  * DeepLink中转页面
@@ -20,98 +14,8 @@ class DeepLinkSoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_deep_link)
 
-        // 首先判断是否是从深度链接跳转过来的
-        val uri = intent.data
-        if (uri != null) {
-            val page = uri.getQueryParameter("page")
-            // 启动app
-            if (Utils.isEmpty(page)) {
-                DeepLinkSoNavigator.launchApp(this)
-                return
-            }
-
-            // 得到指定的配置信息
-            val option = DeepLinkSoClient.config.getOption(page!!)
-            // 如果没有找到相关配置
-            if(option == null){
-                DeepLinkSoClient.config.listener?.onDeepLinkFailed(this,
-                        DeepLinkSoFailedException(DeepLinkSoFailedException.PAGE_NOT_REGISTER,
-                                "The option of the page is not found, have you register it in DeepLinkSo.xml?"),
-                        option)
-                deepLinkFailed()
-            }
-
-            val params = queryParams(option!!, uri)
-
-            if (params == null) {
-                DeepLinkSoClient.config.listener?.onDeepLinkFailed(this,
-                        DeepLinkSoFailedException(DeepLinkSoFailedException.PARAMS_NULL, "param can not be null"),
-                        option)
-                deepLinkFailed()
-                return
-            }
-
-            // 判断是否拦截此次跳转
-            if (needIntercept(option, params)) {
-                DeepLinkSoClient.config.listener?.onDeepLinkFailed(this,
-                        DeepLinkSoFailedException(DeepLinkSoFailedException.INTERCEPT, "failed by intercept"),
-                        option)
-                deepLinkFailed()
-                return
-            }
-
-            DeepLinkSoNavigator.startActivity(this, option, params)
-            finish()
-        }
-
-    }
-
-    private fun queryParams(option: DeepLinkSoOption, uri: Uri): HashMap<String, Any>? {
-        val params = HashMap<String, Any>()
-        if (option.params != null && option.params!!.size > 0) {
-            for (param in option.params!!) {
-                val value = uri.getQueryParameter(param.key)
-                // 目前不允许参数为空
-                if (Utils.isEmpty(value)) {
-                    // 返回null
-                    return null
-                }
-                // 把参数加入到集合中
-                if (param.type == DeepLinkSoConstant.INT) {
-                    params[param.key] = value!!.toInt()
-                } else if (param.type == DeepLinkSoConstant.LONG) {
-                    params[param.key] = value!!.toLong()
-                } else if (param.type == DeepLinkSoConstant.FLOAT) {
-                    params[param.key] = value!!.toFloat()
-                } else if (param.type == DeepLinkSoConstant.DOUBLE) {
-                    params[param.key] = value!!.toDouble()
-                }
-                // 默认是字符串
-                else {
-                    params[param.key] = value!!
-                }
-            }
-        }
-        return params
-    }
-
-    private fun needIntercept(option: DeepLinkSoOption, params: HashMap<String, Any>): Boolean {
-        // 开始调用拦截器
-        val interceptors = DeepLinkSoClient.config.interceptors
-        if (interceptors != null && interceptors.size > 0) {
-            for (interceptor in interceptors) {
-                // 如果某个拦截器需要拦截，之后的拦截器也不会得到执行，页面直接销毁
-                if (interceptor.intercept(this, option, params)) {
-                    return true
-                }
-            }
-        }
-        return false
-    }
-
-    private fun deepLinkFailed() {
-        // 把app回到前台
-        DeepLinkSoNavigator.moveMainTaskToFront(this)
+        DeepLinkSoNavigator.startActivity(this)
         finish()
     }
+
 }
--- library/src/main/java/com/lzp/deeplinkso/utils/DeepLinkSoNavigator.kt
+++ library/src/main/java/com/lzp/deeplinkso/utils/DeepLinkSoNavigator.kt
@@ -1,11 +1,14 @@
 package com.lzp.deeplinkso.utils
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import com.lzp.deeplinkso.DeepLinkSoClient
 import com.lzp.deeplinkso.activity.DeepLinkSoActivity
 import com.lzp.deeplinkso.bean.DeepLinkSoOption
 import com.lzp.deeplinkso.constants.DeepLinkSoConstant
+import com.lzp.deeplinkso.exception.DeepLinkSoFailedException
 
 /**
  * Created by li.zhipeng on 2018/12/6.
@@ -14,6 +17,60 @@ import com.lzp.deeplinkso.constants.DeepLinkSoConstant
  */
 object DeepLinkSoNavigator {
 
+    /**
+     * 通过中转的页面，跳转到指定的页面
+     * */
+    fun startActivity(activity: Activity) {
+        // 首先判断是否是从深度链接跳转过来的
+        val uri = activity.intent.data ?: return
+        val page = uri.getQueryParameter("page")
+        // 启动app
+        if (Utils.isEmpty(page)) {
+            DeepLinkSoNavigator.launchApp(activity)
+            return
+        }
+
+        // 得到指定的配置信息
+        val option = DeepLinkSoClient.config.getOption(page!!)
+        // 如果没有找到相关配置
+        if (option == null) {
+            DeepLinkSoClient.config.listener?.onDeepLinkFailed(activity,
+                    DeepLinkSoFailedException(DeepLinkSoFailedException.PAGE_NOT_REGISTER,
+                            "The option of the page is not found, have you register it in DeepLinkSo.xml?"),
+                    option)
+            deepLinkFailed(activity)
+        }
+
+        val params = queryParams(option!!, uri)
+
+        if (params == null) {
+            DeepLinkSoClient.config.listener?.onDeepLinkFailed(activity,
+                    DeepLinkSoFailedException(DeepLinkSoFailedException.PARAMS_NULL, "param can not be null"),
+                    option)
+            deepLinkFailed(activity)
+            return
+        }
+
+        // 判断是否拦截此次跳转
+        if (needIntercept(activity, option, params)) {
+            DeepLinkSoClient.config.listener?.onDeepLinkFailed(activity,
+                    DeepLinkSoFailedException(DeepLinkSoFailedException.INTERCEPT, "failed by intercept"),
+                    option)
+            deepLinkFailed(activity)
+            return
+        }
+
+        DeepLinkSoNavigator.startActivity(activity, option, params)
+        activity.finish()
+
+    }
+
+    /**
+     * 启动深度链接对应的页面
+     * @param context 跳转的上下文
+     * @param option 深度链接的配置信息
+     * @param params 此次跳转的参数集合
+     * */
     fun startActivity(context: Context, option: DeepLinkSoOption, params: HashMap<String, Any>) {
         val intent = Intent(context, option.className)
         if (option.params?.size ?: 0 > 0) {
@@ -35,7 +92,10 @@ object DeepLinkSoNavigator {
         DeepLinkSoClient.config.listener?.onDeepLinkSuccess(context, option, params)
     }
 
-    fun launchApp(context: Context) {
+    /**
+     * 启动app
+     * */
+    private fun launchApp(context: Context) {
         // 如果没有找到任务栈，启动app
         if (!moveMainTaskToFront(context)) {
             context.startActivity(context.packageManager.getLaunchIntentForPackage(context.packageName))
@@ -43,7 +103,11 @@ object DeepLinkSoNavigator {
         }
     }
 
-    fun moveMainTaskToFront(context: Context): Boolean {
+    /**
+     * 把app移动到前台
+     * */
+    @Suppress("DEPRECATION")
+    private fun moveMainTaskToFront(context: Context): Boolean {
         // 是否已经启动了其他任务栈，如果有，把任务栈移动到前台
         val manager = context.applicationContext.getSystemService(Context.ACTIVITY_SERVICE) as android.app.ActivityManager
         val taskInfo = manager
@@ -59,5 +123,62 @@ object DeepLinkSoNavigator {
             }
         }
         return false
+    }
+
+    /**
+     * 查询此次跳转的参数
+     * */
+    private fun queryParams(option: DeepLinkSoOption, uri: Uri): HashMap<String, Any>? {
+        val params = HashMap<String, Any>()
+        if (option.params != null && option.params!!.size > 0) {
+            for (param in option.params!!) {
+                val value = uri.getQueryParameter(param.key)
+                // 目前不允许参数为空
+                if (Utils.isEmpty(value)) {
+                    // 返回null
+                    return null
+                }
+                // 把参数加入到集合中
+                if (param.type == DeepLinkSoConstant.INT) {
+                    params[param.key] = value!!.toInt()
+                } else if (param.type == DeepLinkSoConstant.LONG) {
+                    params[param.key] = value!!.toLong()
+                } else if (param.type == DeepLinkSoConstant.FLOAT) {
+                    params[param.key] = value!!.toFloat()
+                } else if (param.type == DeepLinkSoConstant.DOUBLE) {
+                    params[param.key] = value!!.toDouble()
+                }
+                // 默认是字符串
+                else {
+                    params[param.key] = value!!
+                }
+            }
+        }
+        return params
+    }
+
+    /**
+     * 是否要拦截此次跳转
+     * */
+    private fun needIntercept(activity: Activity, option: DeepLinkSoOption, params: HashMap<String, Any>): Boolean {
+        // 开始调用拦截器
+        val interceptors = DeepLinkSoClient.config.interceptors
+        if (interceptors != null && interceptors.size > 0) {
+            for (interceptor in interceptors) {
+                // 如果某个拦截器需要拦截，之后的拦截器也不会得到执行，页面直接销毁
+                if (interceptor.intercept(activity, option, params)) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    /**
+     * 启动页面失败
+     * */
+    private fun deepLinkFailed(activity: Activity) {
+        // 把app回到前台
+        DeepLinkSoNavigator.moveMainTaskToFront(activity)
     }
 }
